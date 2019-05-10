34a0a054afd4c5a7d140d7c9e4ce4f99f2f12896
Dmitriy Nazarov <dnazarov@docdoc.ru>
2019-05-10 14:01:48 +0700

    Update front

--- frontend/web/js/global.js
+++ frontend/web/js/global.js
@@ -1,6 +1,2 @@
-jQuery(function () {
-    $(".js-activeOnOff").on("click", function () {
-        $("#" + $(this).data("id")).toggleClass("active")
-    })
-});
+jQuery(function(){$(".js-activeOnOff").on("click",function(){$("#"+$(this).data("id")).toggleClass("active")})});
 //# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImdsb2JhbC5qcyJdLCJuYW1lcyI6WyJqUXVlcnkiLCIkIiwib24iLCJ0aGlzIiwiZGF0YSIsInRvZ2dsZUNsYXNzIl0sIm1hcHBpbmdzIjoiQUFBQUEsT0FBTyxXQUNIQyxFQUFFLG1CQUFtQkMsR0FBRyxRQUFTLFdBQzdCRCxFQUFFLElBQU1BLEVBQUVFLE1BQU1DLEtBQUssT0FBT0MsWUFBWSIsImZpbGUiOiJnbG9iYWwuanMiLCJzb3VyY2VzQ29udGVudCI6WyJqUXVlcnkoZnVuY3Rpb24gKCkge1xyXG4gICAgJChcIi5qcy1hY3RpdmVPbk9mZlwiKS5vbihcImNsaWNrXCIsIGZ1bmN0aW9uICgpIHtcclxuICAgICAgICAkKFwiI1wiICsgJCh0aGlzKS5kYXRhKFwiaWRcIikpLnRvZ2dsZUNsYXNzKFwiYWN0aXZlXCIpO1xyXG4gICAgfSk7XHJcbn0pIl19
