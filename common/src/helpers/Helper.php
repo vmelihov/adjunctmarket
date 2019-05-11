114cee002773b97b8c521831e4f2e3f0a70db772
Dmitriy Nazarov <dnazarov@docdoc.ru>
2019-05-11 15:02:48 +0700

    Helper

---
+++ common/src/helpers/Helper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace common\src\helpers;
+
+use common\models\User;
+use Throwable;
+use Yii;
+use yii\log\Logger;
+use yii\web\IdentityInterface;
+
+class Helper
+{
+    /**
+     * @return IdentityInterface|User
+     */
+    public static function getUserIdentity(): IdentityInterface
+    {
+        try {
+            return Yii::$app->getUser()->getIdentity();
+        } catch (Throwable $e) {
+            Yii::getLogger()->log($e->getMessage(), Logger::LEVEL_ERROR);
+        }
+
+        return null;
+    }
+}
