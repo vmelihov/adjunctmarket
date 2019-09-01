51215a9deaf81172866b89b7ac50fc8582af6b6a
Dmitriy Nazarov <dnazarov@docdoc.ru>
2019-09-01 12:13:31 +0700

    Config

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -1,16 +1,32 @@
 <?php
+
+use yii\swiftmailer\Mailer;
+use yii\db\Connection;
+
 return [
     'components' => [
         'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'class' => Connection::class,
+            'dsn' => 'mysql:host=localhost;dbname=u0565825_instructorshub',
+            'username' => 'u0565825_hub',
+            'password' => 'V3u8F9a2',
             'charset' => 'utf8',
         ],
         'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
+            'class' => Mailer::class,
             'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'username',
+                'password' => 'password',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
         ],
     ],
 ];
