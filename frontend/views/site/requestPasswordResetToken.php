d78b3bf68cf381db157f38d29e95a8cdc518f144
Dmitriy Nazarov <dnazarov@docdoc.ru>
2019-09-04 11:59:40 +0700

    Password reset form

--- frontend/views/site/requestPasswordResetToken.php
+++ frontend/views/site/requestPasswordResetToken.php
@@ -4,29 +4,60 @@
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model PasswordResetRequestForm */
 
-use frontend\models\PasswordResetRequestForm;
+use frontend\assets\AppAsset;
+use frontend\forms\PasswordResetRequestForm;
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use yii\helpers\Url;
 
-$this->title = 'Request password reset';
+$this->title = 'Reset your password';
 $this->params['breadcrumbs'][] = $this->title;
+
+$this->registerCssFile('@web/css/registration.css', ['depends' => [AppAsset::class]]);
+$this->registerJsFile('@web/js/reg.js', ['depends' => [AppAsset::class]]);
+
 ?>
-<div class="site-request-password-reset">
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="p-reg p-login g-content js-regForm">
+    <div class="p-reg__title"><?= Html::encode($this->title) ?></div>
 
-    <p>Please fill out your email. A link to reset password will be sent there.</p>
+    <?php $form = ActiveForm::begin([
+        'id' => 'request-password-reset-form',
+        'options' => ['class' => 'p-reg__form needs-validation'],
+        'fieldConfig' => [
+            'template' => "{input}\n{error}",
+            'options' => [
+                'tag' => false,
+            ],
+        ],
+    ]); ?>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form']); ?>
+    <div class="form-group">
+        <div class="p-reg__form-iblock">
+            <div class="p-reg__form-iblock-icon fal fa-check"></div>
 
-                <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Send', ['class' => 'btn btn-primary']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
+            <?= $form->field($model, 'email')->textInput([
+                'autofocus' => true,
+                'class' => 'p-reg__form-input',
+                'placeholder' => 'Work email address',
+            ]) ?>
         </div>
+    </div>
+
+    <div class="p-reg__form-submit">
+        <?= Html::submitButton('Reset password', ['class' => 'p-reg__form-submit-input']) ?>
+    </div>
+
+    <div class="p-reg__form-login">
+        <a href="<?= Url::to(['/site/login'], true) ?>" class="remembered">I remembered</a>
+    </div>
+    <?php ActiveForm::end(); ?>
+</div>
+
+<div class="p-reg__mail d-none js-regDone">
+    <div class="p-reg__mail-title">
+        We have sent an email with a confirmation link to your email address
+    </div>
+    <div class="p-reg__mail-text">
+        Thank you for choosing our service
     </div>
 </div>
